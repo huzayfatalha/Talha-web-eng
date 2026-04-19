09583c965af062978121e51d22bae3ba450704f2
huzayfatalha <huzayfatalha@gmail.com>
2026-04-19 14:18:50 +0600

    Remove footer copyright text - Clean up footer section

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,5 @@
     <!-- Footer section -->
     <footer>
-        <p>&copy; 2026 Lead Tracking AI. All rights reserved.</p>
     </footer>
     <!-- linking JavaScript file -->
     <script src="/web_project/js/script.js"></script>
